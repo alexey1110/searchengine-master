4ecfbaa9b56d57481304dce5b0257c6c60c3047b
alexey1110 <osipovalexey123@mail.ru>
2025-11-11 01:26:10 +0400

    Added repositories for models and created methods: delete and create in IndexingServiceImpl

---
+++ src/main/java/searchengine/ropositories/PageRepository.java
@@ -0,0 +1,14 @@
+package searchengine.ropositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import searchengine.model.Page;
+
+import java.util.List;
+
+@Repository
+public interface PageRepository extends JpaRepository<Page, Long> {
+    List<Page> findBySiteId(Long siteId);
+
+    void deleteAllBySiteId(Long siteId);
+}
--- src/main/java/searchengine/services/IndexingServiceImpl.java
+++ src/main/java/searchengine/services/IndexingServiceImpl.java
@@ -2,18 +2,50 @@ package searchengine.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import searchengine.config.Site;
 import searchengine.config.SitesList;
 import searchengine.dto.indexing.IndexingResponse;
+import searchengine.ropositories.PageRepository;
+import searchengine.ropositories.SiteRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class IndexingServiceImpl implements IndexingService {
 
-    private final SitesList sites;
+    private final SitesList sitesList;
+    private final SiteRepository siteRepository;
+    private final PageRepository pageRepository;
 
     @Override
     public IndexingResponse startIndexing() {
+        List<Site> sites = sitesList.getSites();
+        for (Site site : sites) {
+            deleteSiteWithPages(site);
+            searchengine.model.Site siteModel = createSite(site);
 
+        }
         return null;
     }
+
+    private void deleteSiteWithPages(Site site) {
+        String url = site.getUrl();
+        searchengine.model.Site siteModel = siteRepository.findByUrl(url);
+        if (siteModel != null) {
+            pageRepository.deleteAllBySiteId(siteModel.getId());
+            siteRepository.delete(siteModel);
+        }
+    }
+
+    private searchengine.model.Site createSite(Site site) {
+        if (site != null) {
+            searchengine.model.Site siteModel = new searchengine.model.Site();
+            siteModel.setUrl(site.getUrl());
+            siteModel.setName(site.getName());
+            return siteRepository.save(siteModel);
+        }
+        return null;
+    }
+
 }
